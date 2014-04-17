970d471e649ef019461a81ec084e2cb5b9977ade
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2014-04-17 01:29:39 -0700

    cleanup

--- TFIDF.py
+++ TFIDF.py
@@ -1,77 +1,52 @@
-# DB
-from pymongo import MongoClient
-
 # Tools
 from persutils import disp, data
 
 # ML
-import nltk
 from collections import Counter
 from math import log
-#import numpy as np
-#from scipy import stats
-#import statsmodels.api as sm
-#from pandas import *
 
-# Connect to DB
+print "Loading data"
 
-client = MongoClient()
-db = client.yelp
-review_collection = db.yelp_academic_dataset_review
+alltoken = data.loadFile('data/alltoken.pkl')
+reviews_feature = data.loadFile('data/reviews_feature.pkl')
+reviews_score = data.loadFile('data/reviews_score.pkl')
 
-# Starting learning
-trainRatio = .8
-n = review_collection.count()
-samplesize = 50000
+n = len(reviews_score)
 
 print "Total reviews:", n
-print "Sample size:", samplesize
-
-reviews_feature = dict()
-reviews_score = dict()
-alltoken = dict()
-
-# extracting tokens
-i = 0
-for review in review_collection.find():
-  i += 1
-  if i == samplesize: break
-
-  disp.tempPrint(str(i))
-
-  text = nltk.tokenize.word_tokenize(review['text'])
-  score = float(review['stars'])
-  
-  reviews_feature[review['review_id']] = Counter(text)
-  reviews_score[review['review_id']] = score
-  
-  for token in text:
-    if token not in alltoken:
-      alltoken[token] = Counter()
-
-    alltoken[token][review['review_id']] += 1
-
-data.saveFile(alltoken, 'data/alltoken.pkl')
-data.saveFile(reviews_feature, 'data/reviews_feature.pkl')
-data.saveFile(reviews_score, 'data/reviews_score.pkl')
 
 # TF-IDF
+print "Computing TF"
 TF = dict()
+i = 0
 for review in reviews_feature:
+  i += 1
+  disp.tempPrint(str(i))
   TF[review] = Counter()
   for token in reviews_feature[review]:
-    TF[review][token] = reviews_feature[review][token] / max(alltoken[token].values())
+    TF[review][token] = float(reviews_feature[review][token]) / float(max(reviews_feature[review].values()))
 
+print "Computing IDF"
 IDF = dict()
+i = 0
 for token in alltoken:
-  DF[token] = log(n / len(alltoken[token]))
+  i += 1
+  disp.tempPrint(str(i))
+  IDF[token] = log(float(n) / float(len(alltoken[token])))
 
+print "Computing TFIDF"
 TFIDF = dict()
+i = 0
 for review in reviews_feature:
+  i += 1
+  disp.tempPrint(str(i))
   TFIDF[review] = Counter()
   for token in reviews_feature[review]:
-    TFIDF[review][token] = TF[review][token] * DF[token]
+    TFIDF[review][token] = TF[review][token] * IDF[token]
 
 # SGD linear regression
 
 data.saveFile(TFIDF, 'data/TFIDF.pkl')
+
+# Testing
+# print TFIDF[reviews_feature.keys()[0]]
