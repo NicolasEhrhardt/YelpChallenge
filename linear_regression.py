bd01426ad0278092bbfd742887a359b83642ceb0
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2014-04-17 10:45:17 -0700

    cleanup

--- linear_regression.py
+++ linear_regression.py
@@ -1,17 +1,18 @@
 # Tools 
 from persutils import data
-from sgd import sgd;
+from linear_regression_sgd import sgd
 
 print "loading review scores"
-target = data.loadFile('data/reviews_score.pkl');
+target = data.loadFile('data/reviews_score.pkl')
 print "loading TFIDF matrix"
-TFIDF = data.loadFile('data/TFIDF.pkl');
+TFIDF = data.loadFile('data/TFIDF.pkl')
 
-nReviews = len(target);
+nReviews = len(target)
 
-alphas = [0.001, 0.01, 0.1];
+alphas = [0.001]#, 0.01, 0.1]
 
 print "Optimizing with SGD"
-for alpha in alphas : 
-  results = sgd(TFIDF, target, alpha);
-  print "Alpha = ", alpha, " -- RMSE = ", results['RMSE'];
+for alpha in alphas: 
+  RMSE, weights, bias = sgd(TFIDF, target, alpha)
+  print "Alpha = ", alpha, " -- RMSE = ", RMSE
+  data.saveFile((weights, bias), "linear_regression_weights_alpha_" + str(alpha) + ".pkl")
