f9448eb322f78fd42cb381e687cd36d948c66e2d
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2014-05-29 13:48:11 -0700

    cleanup

--- linear_regression/linear_regression_analysis.py
+++
@@ -1,9 +0,0 @@
-# Tools 
-from utils import data
-import evaluation
-
-root = data.getParent(__file__)
-
-print "Biggest weights"
-weights, bias = data.loadFile(root + "/computed/linear_regression_weights.pkl")
-print "\n".join(map(lambda t: str(t[1]) + " - " + str(t[0]), weights.most_common(100)))
