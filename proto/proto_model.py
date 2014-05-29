d72aa795760c63ecf10dc72330db0dfeb31ee636
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2014-05-29 14:21:42 -0700

    take valid set from training

--- proto/proto_model.py
+++ proto/proto_model.py
@@ -72,25 +72,35 @@ print("- Number of docs leftout: %d" % (leftouts_holdout + leftouts_train))
 
 print("< Document vectors loaded")
 
-print("> Create training set")
-X_train = np.concatenate(X_train, axis=0)
-Y_train = np.array(Y_train)
-assert(X_train.shape[0] == len(Y_train))
+print("> Create all training set")
+X_train_all = np.concatenate(X_train, axis=0)
+Y_train_all = np.array(Y_train)
+assert(X_train_all.shape[0] == len(Y_train_all))
 
 print("> Create holdout set")
 X_holdout = np.concatenate(X_holdout, axis=0)
 Y_holdout = np.array(Y_holdout)
 assert(X_holdout.shape[0] == len(Y_holdout))
 
-permut = np.random.permutation(len(Y_holdout))
+
 print("- Create valid set")
-X_valid = X_holdout[permut[0:len(permut)/2], :]
-Y_valid = Y_holdout[permut[0:len(permut)/2]]
+permut = np.random.permutation(len(Y_train))
+validsz = 50000
+
+X_valid = X_train_all[permut[0:validsz], :]
+Y_valid = Y_train_all[permut[0:validsz]]
+print("- valid set of size: %d" % len(Y_valid))
+
+X_train = X_train_all[permut[validsz::], :]
+Y_train = Y_train_all[permut[validsz::]]
+print("- train set of size: %d" % len(Y_train))
+
 assert(X_valid.shape[0] == len(Y_valid))
+assert(X_train.shape[0] == len(Y_train))
 
 print "- Create test subet"
-X_test = X_holdout[permut[len(permut)/2::], :]
-Y_test = Y_holdout[permut[len(permut)/2::]]
+X_test = X_holdout
+Y_test = Y_holdout
 assert(X_test.shape[0] == len(Y_test))
 print "< Test sets created"
 
